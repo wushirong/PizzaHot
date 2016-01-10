4e06cd9cbf986deb24bd79541ab7f30a0a4004a3
skylar <cy@wushirongdeMacBook-Pro.local>
2016-01-10 13:46:25 -0500

    Task I

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   private
+  
     def current_cart
     	Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
     	cart = Cart.create
     	session[:cart_id] = cart.id
     	cart
+    end
+
+  protected
+
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order(:name)
+
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json { render json: @users}
+    end
   end
 
   # GET /users/1
@@ -28,11 +33,14 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to users_url,
+                      notice: "User #{@user.name} was successfully created." }
+        format.json { render json: @user, 
+          status: :created, location: @user }
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, 
+          status: :unprocessable_entity }
       end
     end
   end
@@ -40,13 +48,17 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    @user = User.find(params[:id])
+
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to users_url,
+          notice: "User #{@user.name} was successfully updated." }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, 
+          status: :unprocessable_entity }
       end
     end
   end
@@ -55,6 +67,13 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
+    begin
+      @user.destroy
+      flash[:notice] = "User #{@user.name} deleted"
+    rescue Exception => e
+      flash[:notice] = e.message
+    end
+
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
@@ -69,6 +88,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :password_digest)
+      params.require(:user).permit(:name, :password)
     end
 end
