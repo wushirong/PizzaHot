739ab6da93518026dad76cb49b7f51fbb28b5af2
skylar <cy@wushirongdeMacBook-Pro.local>
2016-01-10 09:32:31 -0500

    Task H

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,29 @@
+class OrderNotifier < ApplicationMailer
+  default from: 'Sam Ruby <depot@example.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  def receiver(order)
+    @order = order
+
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+  end
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped
+    @order = order
+
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
+  end
+end
